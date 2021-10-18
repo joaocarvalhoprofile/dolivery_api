282df00b19f014e1144c8693d7c8986b8172cf9c
Joao Carvalho <joaokvalho@gmail.com>
2021-10-18 14:16:05 -0300

    feat: Add schema User

---
+++ lib/dolivery_api/users/user.ex
@@ -0,0 +1,44 @@
+defmodule DoliveryApi.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:name, :email, :password, :cpf, :age, :cep, :address]
+
+  @derive {Jason.Encoder, only: [:id, :name, :age, :email, :cpf, :cep, :address]}
+
+  schema "user" do
+    field :name, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+    field :cpf, :string
+    field :age, :integer
+    field :cep, :string
+    field :address, :string
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 6)
+    |> validate_length(:cep, is: 8)
+    |> validate_length(:cpf, is: 11)
+    |> unique_constraint([:email])
+    |> unique_constraint([:cpf])
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Pbkdf2.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
+end
