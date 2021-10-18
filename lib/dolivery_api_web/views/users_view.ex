b4c6760093ced229999b428756af194c4a306e0c
Joao Carvalho <joaokvalho@gmail.com>
2021-10-18 15:29:10 -0300

    feat: Add users_view

---
+++ lib/dolivery_api_web/views/users_view.ex
@@ -0,0 +1,14 @@
+defmodule DoliveryApiWeb.UsersView do
+  use DoliveryApiWeb, :view
+
+  alias DoliveryApi.Domain.Users.User
+
+  def render("create.json", %{user: %User{} = user}) do
+    %{
+      messge: "User created",
+      user: user
+    }
+  end
+
+  def render("user.json", %{user: %User{} = user}), do: %{user: user}
+end
