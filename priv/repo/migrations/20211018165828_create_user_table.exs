a8b1f69a19113eb32135d54eff76609f2d0b77d2
Joao Carvalho <joaokvalho@gmail.com>
2021-10-18 13:59:47 -0300

    feat: Add Migration user

---
+++ priv/repo/migrations/20211018165828_create_user_table.exs
@@ -0,0 +1,19 @@
+defmodule DoliveryApi.Repo.Migrations.CreateUserTable do
+  use Ecto.Migration
+
+  def change do
+    create table :user do
+      add :name, :string
+      add :email, :string
+      add :password_hash, :string
+      add :cpf, :string
+      add :age, :integer
+      add :cep, :string
+      add :address, :string
+
+      timestamps()
+    end
+
+    create unique_index(:user, [:cpf])
+    create unique_index(:user, [:email])
+end
