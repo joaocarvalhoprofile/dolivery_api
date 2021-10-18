12e5bc5f0befd63568b0511f7d0cea049b74bcbd
Joao Carvalho <joaokvalho@gmail.com>
2021-10-18 13:56:08 -0300

    chore: Add libs credo, excoveralls, exmachina and config credo

--- mix.exs
+++ mix.exs
@@ -10,7 +10,14 @@ defmodule DoliveryApi.MixProject do
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -44,7 +51,11 @@ defmodule DoliveryApi.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:pbkdf2_elixir, "~> 1.4"},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:ex_machina, "~> 2.7.0"}
     ]
   end
 
