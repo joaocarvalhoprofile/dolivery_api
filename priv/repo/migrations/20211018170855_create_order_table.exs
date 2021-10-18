994c081903cdd16b37ecd1449c7406ac6e6e250d
Joao Carvalho <joaokvalho@gmail.com>
2021-10-18 14:10:11 -0300

    feat: Add migration Order

---
+++ priv/repo/migrations/20211018170855_create_order_table.exs
@@ -0,0 +1,13 @@
+defmodule DoliveryApi.Repo.Migrations.CreateOrderTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:order) do
+      add :user_id, references(:user, type: :binary_id)
+      add :address, :string
+      add :comments, :string
+      add :payment_method, :payment_method
+
+      timestamps()
+  end
+end
